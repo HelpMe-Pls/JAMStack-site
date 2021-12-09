42e98ffb5cbcb0ffa382f922f4c8384f33f598a7
Random Guy <leekhoi5319990@gmail.com>
2021-12-09 12:39:50 +0700

    Refractor footer

--- frontend/src/components/ui/footer.js
+++ frontend/src/components/ui/footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
-//import IconButton from "@material-ui/core/IconButton"
+import IconButton from "@material-ui/core/IconButton"
 import { makeStyles } from "@material-ui/core/styles"
 import { Link } from "gatsby"
 
@@ -30,10 +30,18 @@ const useStyles = makeStyles(theme => ({
 			marginBottom: "3rem",
 		},
 	},
+	icon: {
+		"&:hover": {
+			backgroundColor: "transparent",
+		},
+	},
 	"@global": {
 		body: {
 			// to remove default <body> margin of the browser
 			margin: 0,
+		},
+		a: {
+			textDecoration: "none",
 		},
 	},
 }))
@@ -41,111 +49,106 @@ const useStyles = makeStyles(theme => ({
 export default function Footer() {
 	const classes = useStyles()
 
+	const socialMedia = [
+		{ icon: facebook, alt: "facebook", link: "https://facebook.com/me" },
+		{ icon: twitter, alt: "twitter", link: "https://twitter.com" },
+		{ icon: instagram, alt: "instagram", link: "https://instagram.com" },
+	]
+
+	const routes = {
+		// setting this structure as an array will make it harder to read & maping through, that's why we set it as an object
+		"Contact Us": [
+			// space between words of the key is unacceptable, that's why it has to be put as a string
+			{ label: "(+84) 369 149 942", href: "tel:(+84) 369 149 942" },
+			{
+				label: "khoile5399@gmail.com",
+				href: "mailto:khoile5399@gmail.com",
+			},
+		],
+		"Customer Service": [
+			{ label: "Contact Us", path: "/contact" },
+			{ label: "My Account", path: "/account" },
+		],
+		Information: [
+			{ label: "Privacy Policy", path: "/privacy-policy" },
+			{ label: "Terms and Conditions", path: "/terms-conditions" },
+		],
+	}
+
 	return (
 		<footer className={classes.footer}>
 			<Grid container justify="space-between">
 				{/* for links */}
-
 				<Grid item classes={{ root: classes.linkContainer }}>
 					{/* {justify} only works for {item}, that's why we have to wrap the below {container} in this {item}*/}
 					<Grid container>
-						<Grid
-							container // inherit the width of the parent (100%)
-							item
-							direction="column"
-							classes={{ root: classes.linkColumn }} // so that's why we have explicitly set the width of each child container
-						>
-							<Grid item>
-								<Typography variant="h5">Contact Us</Typography>
+						{Object.keys(routes).map((column, i) => (
+							<Grid
+								item
+								key={i}
+								container
+								direction="column"
+								classes={{ root: classes.linkColumn }}
+							>
+								<Grid item>
+									<Typography variant="h5">
+										{column}
+									</Typography>
+								</Grid>
+								{routes[column].map(
+									// routes[column] as the computed property of the object
+									row => (
+										<Grid item key={row.label}>
+											<Typography
+												component={
+													row.path ? Link : "a"
+												}
+												to={
+													row.path
+														? row.path
+														: undefined // so that it only renders the link if it has a path
+												}
+												href={
+													row.href
+														? row.href
+														: undefined
+												}
+												variant="body1"
+												classes={{
+													body1: classes.link,
+												}}
+											>
+												{row.label}
+											</Typography>
+										</Grid>
+									)
+								)}
 							</Grid>
-							<Grid item>
-								<Typography
-									variant="body1"
-									classes={{ body1: classes.link }} // https://mui.com/api/typography/#css
-								>
-									randommail@gmail.com
-								</Typography>
-							</Grid>
-							<Grid item>
-								<Typography
-									variant="body1"
-									classes={{ body1: classes.link }}
-								>
-									(696) 669-9669
-								</Typography>
-							</Grid>
-						</Grid>
-						<Grid
-							container
-							item
-							direction="column"
-							classes={{ root: classes.linkColumn }}
-						>
-							<Grid item>
-								<Typography variant="h5">
-									Customer Service
-								</Typography>
-							</Grid>
-							<Grid item>
-								<Typography
-									variant="body1"
-									classes={{ body1: classes.link }}
-								>
-									Contact Us
-								</Typography>
-							</Grid>
-							<Grid item>
-								<Typography
-									variant="body1"
-									classes={{ body1: classes.link }}
-								>
-									My Account
-								</Typography>
-							</Grid>
-						</Grid>
-						<Grid
-							container
-							item
-							direction="column"
-							classes={{ root: classes.linkColumn }}
-						>
-							<Grid item>
-								<Typography variant="h5">
-									Information
-								</Typography>
-							</Grid>
-							<Grid item>
-								<Typography
-									variant="body1"
-									classes={{ body1: classes.link }}
-								>
-									Privacy Policy
-								</Typography>
-							</Grid>
-							<Grid item>
-								<Typography
-									variant="body1"
-									classes={{ body1: classes.link }}
-								>
-									Terms & Conditions
-								</Typography>
-							</Grid>
-						</Grid>
+						))}
 					</Grid>
 				</Grid>
 
 				{/* for social media */}
 				<Grid item>
 					<Grid container item direction="column" alignItems="center">
-						<Grid item>
-							<img src={facebook} alt="facebook" />
-						</Grid>
-						<Grid item classes={{ root: classes.spacer }}>
-							<img src={twitter} alt="twitter" />
-						</Grid>
-						<Grid item>
-							<img src={instagram} alt="instagram" />
-						</Grid>
+						{socialMedia.map(platform => (
+							<Grid item key={platform.alt}>
+								<IconButton
+									//onClick={() => window.open(platform.link)}
+									classes={{ root: classes.icon }}
+									component="a"
+									disableRipple
+									href={platform.link}
+									target="_blank"
+									rel="noreferrer noopener"
+								>
+									<img
+										src={platform.icon}
+										alt={platform.alt}
+									/>
+								</IconButton>
+							</Grid>
+						))}
 					</Grid>
 				</Grid>
 			</Grid>
