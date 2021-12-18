639074ce0d2e1595d5c3d0cb8fd8dd9b8d0d46d0
Lê Tấn Khôi <43715386+HelpMe-Pls@users.noreply.github.com>
2021-12-18 22:13:18 +0700

    Basic layout for Contact Page (#2)
    * Styling Contact Info
    
    * Scaffolding Input Fields
    
    * Styling Input Fields
    
    Using reusable customized SVG components

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,23 +1,27 @@
-import * as React from "react"
+import React, { useState } from "react"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
+import TextField from "@material-ui/core/TextField"
+import InputAdornment from "@material-ui/core/InputAdornment"
 import Button from "@material-ui/core/Button"
 import clsx from "clsx"
 import { makeStyles, useTheme } from "@material-ui/core/styles"
-import { Link } from "gatsby"
+//import { Link } from "gatsby"
 
 import address from "../images/address.svg"
-import phone from "../images/phone-adornment.svg"
-import Email from "../images/EmailAdornment.js"
 import send from "../images/send.svg"
+import nameAdornment from "../images/name-adornment.svg"
+
+import Email from "../images/EmailAdornment.js"
+import Phone from "../images/PhoneAdornment.js"
 
 import Layout from "../components/ui/layout"
 
 const useStyles = makeStyles(theme => ({
 	mainContainer: {
-		height: "45rem",
+		height: "35rem",
 		backgroundColor: theme.palette.primary.main,
-		marginBottom: "10rem",
+		margin: "10rem 0",
 		[theme.breakpoints.down("md")]: {
 			marginTop: "8rem",
 			height: "90rem",
@@ -62,9 +66,6 @@ const useStyles = makeStyles(theme => ({
 			backgroundColor: theme.palette.secondary.light,
 		},
 	},
-	sendButton: {
-		textTransform: "none",
-	},
 	sendIcon: {
 		marginLeft: "2rem",
 	},
@@ -75,12 +76,10 @@ const useStyles = makeStyles(theme => ({
 	contactIcon: {
 		height: "3rem",
 		width: "3rem",
-		marginRight: "2rem",
 	},
 	contactEmailIcon: {
 		height: "2.25rem",
 		width: "3rem",
-		marginRight: "2rem",
 	},
 	infoContainer: {
 		height: "21.25rem",
@@ -91,13 +90,78 @@ const useStyles = makeStyles(theme => ({
 	middleInfo: {
 		borderTop: "2px solid #fff",
 		borderBottom: "2px solid #fff",
-		padding: "1rem 0",
+	},
+	iconContainer: {
+		borderRight: "2px solid #fff",
+		height: "7rem",
+		width: "8rem",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		// [theme.breakpoints.down("xs")]: {
+		//   height: "5rem",
+		//   width: "6rem",
+		// },
+	},
+	textField: {
+		width: "30rem",
+		// [theme.breakpoints.down("sm")]: {
+		//   width: "20rem",
+		// },
+	},
+	input: {
+		color: "#fff",
+	},
+	emailAdornment: {
+		height: 17,
+		width: 22,
+		marginBottom: 10,
+	},
+	phoneAdornment: {
+		width: 25.173,
+		height: 25.122,
+	},
+	fieldContainer: {
+		marginBottom: "1rem",
+	},
+	multilineContainer: {
+		marginTop: "1rem",
+	},
+
+	// use Inspect Element with @global styling to apply styles for very specific elements
+	// that you may not be able to figure it out in your code
+	// these styles are always applied even if their classes aren't mentioned in the component
+	// well, in this case they're actually located in the <TextField> but it's abstracted away
+	// due to compound styling that are applied to the <TextField>'s {InputProp} (https://mui.com/api/text-field/#props),
+	// therefore it's hard to track what specific CSS class it is >> using Inspect Element is a faster way to find it
+	"@global": {
+		// Inspect element (of the form's input) >> find the <div> where it says ::before && ::after
+		// Click on that <div> >> go to Styles >> Scroll down >> you'll find these styles
+		".MuiInput-underline:before, .MuiInput-underline:hover:not(.Mui-disabled):before":
+			{
+				borderBottom: "2px solid #fff",
+			},
+		".MuiInput-underline:after": {
+			borderBottom: `2px solid ${theme.palette.secondary.main}`,
+		},
+
+		// https://mui.com/api/input/#css
+		".MuiInput-multiline": {
+			border: "2px solid #fff",
+			borderRadius: 10,
+			padding: "1rem",
+		},
 	},
 }))
 
 const ContactPage = () => {
 	const classes = useStyles()
-	//const theme = useTheme()
+	const theme = useTheme()
+
+	const [name, setName] = useState("")
+	const [email, setEmail] = useState("")
+	const [phoneNumber, setPhoneNumber] = useState("")
+	const [message, setMessage] = useState("")
 
 	// const matchesMD = useMediaQuery(theme => theme.breakpoints.down("md"))
 	// const matchesXS = useMediaQuery(theme => theme.breakpoints.down("xs"))
@@ -130,8 +194,120 @@ const ContactPage = () => {
 						>
 							<Typography variant="h4">Contact Us</Typography>
 						</Grid>
+						<Grid item>
+							<Grid container direction="column">
+								<Grid
+									item
+									classes={{ root: classes.fieldContainer }}
+								>
+									<TextField
+										value={name}
+										onChange={e => setName(e.target.value)}
+										placeholder="Name"
+										classes={{ root: classes.textField }}
+										InputProps={{
+											classes: { input: classes.input },
+											startAdornment: (
+												<InputAdornment position="start">
+													<img
+														src={nameAdornment}
+														alt="name"
+													/>
+												</InputAdornment>
+											),
+										}}
+									/>
+								</Grid>
+								<Grid
+									item
+									classes={{ root: classes.fieldContainer }}
+								>
+									<TextField
+										value={email}
+										onChange={e => setEmail(e.target.value)}
+										placeholder="Email"
+										classes={{ root: classes.textField }}
+										InputProps={{
+											classes: { input: classes.input },
+											startAdornment: (
+												<InputAdornment position="start">
+													<div
+														className={
+															classes.emailAdornment
+														}
+													>
+														<Email
+															color={
+																theme.palette
+																	.secondary
+																	.main
+															}
+														/>
+													</div>
+												</InputAdornment>
+											),
+										}}
+									/>
+								</Grid>
+								<Grid
+									item
+									classes={{ root: classes.fieldContainer }}
+								>
+									<TextField
+										value={phoneNumber}
+										onChange={e =>
+											setPhoneNumber(e.target.value)
+										}
+										placeholder="Phone Number"
+										classes={{ root: classes.textField }}
+										InputProps={{
+											classes: { input: classes.input },
+											startAdornment: (
+												<InputAdornment position="start">
+													<div
+														className={
+															classes.phoneAdornment
+														}
+													>
+														<Phone
+															color={
+																theme.palette
+																	.secondary
+																	.main
+															}
+														/>
+													</div>
+												</InputAdornment>
+											),
+										}}
+									/>
+								</Grid>
+								<Grid
+									item
+									classes={{
+										root: classes.multilineContainer,
+									}}
+								>
+									<TextField
+										value={message}
+										onChange={e =>
+											setMessage(e.target.value)
+										}
+										placeholder="Message"
+										classes={{ root: classes.textField }}
+										multiline
+										rows={8}
+										InputProps={{
+											disableUnderline: true,
+											classes: { input: classes.input },
+										}}
+									/>
+								</Grid>
+							</Grid>
+						</Grid>
 						<Grid
 							item
+							component={Button}
 							classes={{
 								root: clsx(
 									classes.buttonContainer,
@@ -139,16 +315,12 @@ const ContactPage = () => {
 								),
 							}}
 						>
-							<Button classes={{ root: classes.sendButton }}>
-								<Typography variant="h4">
-									send message
-								</Typography>
-								<img
-									src={send}
-									className={classes.sendIcon}
-									alt="send message"
-								/>
-							</Button>
+							<Typography variant="h4">Send message</Typography>
+							<img
+								src={send}
+								className={classes.sendIcon}
+								alt="send message"
+							/>
 						</Grid>
 					</Grid>
 				</Grid>
@@ -162,7 +334,10 @@ const ContactPage = () => {
 						classes={{ root: classes.infoContainer }}
 					>
 						<Grid item container alignItems="center">
-							<Grid item>
+							<Grid
+								item
+								classes={{ root: classes.iconContainer }}
+							>
 								<img
 									src={address}
 									className={classes.contactIcon}
@@ -184,12 +359,13 @@ const ContactPage = () => {
 							alignItems="center"
 							classes={{ root: classes.middleInfo }}
 						>
-							<Grid item>
-								<img
-									src={phone}
-									className={classes.contactIcon}
-									alt="phone"
-								/>
+							<Grid
+								item
+								classes={{ root: classes.iconContainer }}
+							>
+								<div className={classes.contactIcon}>
+									<Phone />
+								</div>
 							</Grid>
 							<Grid item>
 								<Typography
@@ -203,9 +379,13 @@ const ContactPage = () => {
 						<Grid item container alignItems="center">
 							<Grid
 								item
-								classes={{ root: classes.contactEmailIcon }}
+								classes={{ root: classes.iconContainer }}
 							>
-								<Email color="#fff" />
+								{/* the customized SVG icon couldn't be displayed (scaled) without being
+								contained in a parent with set dimensions */}
+								<div className={classes.contactEmailIcon}>
+									<Email color="#fff" />
+								</div>
 							</Grid>
 							<Grid item>
 								<Typography
