7f8402546cb04c2ca1ee558ed32dc289b0c6a1b4
Random Guy <leekhoi5319990@gmail.com>
2021-12-18 13:09:55 +0700

    Scaffolding Input Fields

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,10 +1,11 @@
-import * as React from "react"
+import React, { useState } from "react"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
+import TextField from "@material-ui/core/TextField"
 import Button from "@material-ui/core/Button"
 import clsx from "clsx"
-import { makeStyles, useTheme } from "@material-ui/core/styles"
-import { Link } from "gatsby"
+import { makeStyles } from "@material-ui/core/styles"
+//import { Link } from "gatsby"
 
 import address from "../images/address.svg"
 import phone from "../images/phone-adornment.svg"
@@ -59,7 +60,7 @@ const useStyles = makeStyles(theme => ({
 		textTransform: "none",
 		borderRadius: 0,
 		"&:hover": {
-			backgroundColor: theme.palette.secondary.dark,
+			backgroundColor: theme.palette.secondary.light,
 		},
 	},
 	sendIcon: {
@@ -74,7 +75,7 @@ const useStyles = makeStyles(theme => ({
 		width: "3rem",
 	},
 	contactEmailIcon: {
-		height: "2.25rem",
+		height: "2.25rem", // the customized SVG icon couldn't be displayed (scaled) without having this set
 		width: "3rem",
 	},
 	infoContainer: {
@@ -99,11 +100,55 @@ const useStyles = makeStyles(theme => ({
 		//   width: "6rem",
 		// },
 	},
+	textField: {
+		width: "30rem",
+		// [theme.breakpoints.down("sm")]: {
+		//   width: "20rem",
+		// },
+	},
+	input: {
+		color: "#fff",
+	},
+	fieldContainer: {
+		marginBottom: "1rem",
+	},
+	multilineContainer: {
+		marginTop: "1rem",
+	},
+
+	// use Inspect Element with @global styling to apply styles for very specific elements
+	// that you may not be able to figure it out in your code
+	// these styles are always applied even if their classes aren't mentioned in the component
+	// well, in this case they're actually located in the <TextField> but it's abstracted away due to compound styling that are applied to the <TextField>'s {InputProp} (https://mui.com/api/text-field/#props),
+	// therefore it's hard to track what specific CSS class it is >> using Inspect Element is a faster way to find it
+	"@global": {
+		// Inspect element (of the form's input) >> find the <div> where it says ::before && ::after
+		// Click on that <div> >> go to Styles >> Scroll down >> you'll find these styles
+		".MuiInput-underline:before, .MuiInput-underline:hover:not(.Mui-disabled):before":
+			{
+				borderBottom: "2px solid #fff",
+			},
+		".MuiInput-underline:after": {
+			borderBottom: `2px solid ${theme.palette.secondary.main}`,
+		},
+
+		// https://mui.com/api/input/#css
+		".MuiInput-multiline": {
+			border: "2px solid #fff",
+			borderRadius: 10,
+			padding: "1rem",
+		},
+	},
 }))
 
 const ContactPage = () => {
 	const classes = useStyles()
 	//const theme = useTheme()
+
+	const [name, setName] = useState("")
+	const [email, setEmail] = useState("")
+	const [phoneNumber, setPhoneNumber] = useState("")
+	const [message, setMessage] = useState("")
 
 	// const matchesMD = useMediaQuery(theme => theme.breakpoints.down("md"))
 	// const matchesXS = useMediaQuery(theme => theme.breakpoints.down("xs"))
@@ -135,6 +180,75 @@ const ContactPage = () => {
 							}}
 						>
 							<Typography variant="h4">Contact Us</Typography>
+						</Grid>
+						<Grid item>
+							<Grid container direction="column">
+								<Grid
+									item
+									classes={{ root: classes.fieldContainer }}
+								>
+									<TextField
+										value={name}
+										onChange={e => setName(e.target.value)}
+										placeholder="Name"
+										classes={{ root: classes.textField }}
+										InputProps={{
+											classes: { input: classes.input },
+										}}
+									/>
+								</Grid>
+								<Grid
+									item
+									classes={{ root: classes.fieldContainer }}
+								>
+									<TextField
+										value={email}
+										onChange={e => setEmail(e.target.value)}
+										placeholder="Email"
+										classes={{ root: classes.textField }}
+										InputProps={{
+											classes: { input: classes.input },
+										}}
+									/>
+								</Grid>
+								<Grid
+									item
+									classes={{ root: classes.fieldContainer }}
+								>
+									<TextField
+										value={phoneNumber}
+										onChange={e =>
+											setPhoneNumber(e.target.value)
+										}
+										placeholder="Phone Number"
+										classes={{ root: classes.textField }}
+										InputProps={{
+											classes: { input: classes.input },
+										}}
+									/>
+								</Grid>
+								<Grid
+									item
+									classes={{
+										root: classes.multilineContainer,
+									}}
+								>
+									<TextField
+										value={message}
+										onChange={e =>
+											setMessage(e.target.value)
+										}
+										placeholder="Message"
+										classes={{ root: classes.textField }}
+										multiline
+										rows={8}
+										InputProps={{
+											disableUnderline: true,
+											classes: { input: classes.input },
+										}}
+									/>
+								</Grid>
+							</Grid>
 						</Grid>
 						<Grid
 							item
