70a017cd3c93fdb453eeb2327a08d493213276e3
Random Guy <leekhoi5319990@gmail.com>
2022-02-12 14:48:58 +0700

    AWS

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,23 +1,23 @@
 module.exports = ({ env }) => ({
-  email: {
-    provider: "sendgrid",
-    providerOptions: {
-      apiKey: env("SENDGRID_API_KEY"),
-    },
-    settings: {
-      defaultFrom: "leekhoi5319990@gmail.com",
-      defaultTo: "leekhoi5319990@gmail.com",
-    },
-  },
-  //   upload: {
-  //     provider: "aws-s3",
-  //     providerOptions: {
-  //       accessKeyId: env("AWS_ACCESS_KEY_ID"),
-  //       secretAccessKey: env("AWS_ACCESS_SECRET"),
-  //       region: env("AWS_REGION"),
-  //       params: {
-  //         Bucket: env("AWS_BUCKET_NAME"),
-  //       },
-  //     },
-  //   },
+	email: {
+		provider: "sendgrid",
+		providerOptions: {
+			apiKey: env("SENDGRID_API_KEY"),
+		},
+		settings: {
+			defaultFrom: "leekhoi5319990@gmail.com",
+			defaultTo: "leekhoi5319990@gmail.com",
+		},
+	},
+	upload: {
+		provider: "aws-s3",
+		providerOptions: {
+			accessKeyId: env("AWS_ACCESS_KEY_ID"),
+			secretAccessKey: env("AWS_ACCESS_SECRET"),
+			region: env("AWS_REGION"),
+			params: {
+				Bucket: env("AWS_BUCKET_NAME"),
+			},
+		},
+	},
 });
