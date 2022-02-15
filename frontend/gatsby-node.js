07a3d9bc1b444001a6d6a53d1e3aeffc27442b96
Random Guy <leekhoi5319990@gmail.com>
2022-02-15 12:06:34 +0700

    Fix build failed with "react-spring' and 'react-lottie'

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -92,6 +92,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
 			module: {
 				rules: [
 					{ test: /react-spring-3d-carousel/, use: loaders.null() },
+					{ test: /react-lottie/, use: loaders.null() },
 				],
 			},
 		})
